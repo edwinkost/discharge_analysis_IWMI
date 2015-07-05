197b52703e395d690e57f4bcb9175d5ff567a10a
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-07-05 23:48:14 +0200

    Find the best run.

--- find_the_best_runs.R
+++ find_the_best_runs.R
@@ -25,7 +25,7 @@ ns_discharge[which(ns_discharge < 0.00)] = 0.00
 average_ns_discharge = mean(ns_discharge, na.rm = TRUE)
 #
 baseflow_deviation_relative = abs(baseflow_table$avg_baseflow_deviation/baseflow_table$average_iwmi_opt_baseflow)
-baseflow_deviation_relative[which(baseflow_deviation_relative > 1.50)] = 1.50
+baseflow_deviation_relative[which(baseflow_deviation_relative > 1.00)] = 1.00
 baseflow_deviation = mean(baseflow_deviation_relative, na.rm = TRUE)
 baseflow_deviation = floor(baseflow_deviation*10)/10
 #
