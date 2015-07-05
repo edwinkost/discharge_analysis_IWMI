99f0b8f4f8852ff025860f7fd5083ea6de8759ad
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-07-05 23:43:40 +0200

    Find the best run.

--- find_the_best_runs.R
+++ find_the_best_runs.R
@@ -1,9 +1,11 @@
 
 # global folder for all scenarios
-global_folder = "/projects/wtrcycle/users/edwinhs/IWMI_calibration/"
+#~ global_folder = "/projects/wtrcycle/users/edwinhs/IWMI_calibration/"
+global_folder = "/scratch/edwin/30min_22_jun_2015/rerun_for_iwmi/calibration_27_june_2015/"
 
 first_run_to_be_analyzed = TRUE
-for (i in seq(0,224,1)) {
+#~ for (i in seq(0,224,1)) {
+for (i in seq(0,74,1)) {
 
 # run code/name:
 run_code = paste("code__a__",i,sep="")
@@ -46,11 +48,12 @@ summary_df$average_ns_discharge = as.numeric(as.character(summary_df$average_ns_
 summary_df$general_performance  = as.numeric(as.character(summary_df$general_performance))
 
 # merge it to parameters table
-parameter_table_1st = read.table("/home/edwinhs/github/edwinkost/calibration_log_IWMI/runs_for_IWMI_calibration_code_A/table_24_nov_2014.txt",header=TRUE)
-parameter_table_2nd = read.table("/home/edwinhs/github/edwinkost/calibration_log_IWMI/runs_for_IWMI_calibration_code_A/table_26_nov_2014.txt",header=TRUE)
+parameter_table_1st = read.table("/home/sutan101/github/edwinkost/calibration_log_IWMI/runs_for_IWMI_calibration_code_AA/table_27_june_2014_speedy.txt",header=TRUE)
+parameter_table_2nd = read.table("/home/sutan101/github/edwinkost/calibration_log_IWMI/runs_for_IWMI_calibration_code_AA/table_27_june_2014_speedy_2.txt",header=TRUE)
+parameter_table_3rd = read.table("/home/sutan101/github/edwinkost/calibration_log_IWMI/runs_for_IWMI_calibration_code_AA/table_30_june_2014_cartesius.txt",header=TRUE)
 reference_run = c("code__a__0",1.0,0.0,0.0,1.0)
 #
-parameter_table = rbind(reference_run, parameter_table_1st, parameter_table_2nd) 
+parameter_table = rbind(reference_run, parameter_table_1st, parameter_table_2nd, parameter_table_3rd) 
 parameter_table[ ,1] <- as.character(parameter_table[,1])
 parameter_table[1,1] <- "code__a__0"
 parameter_table[ ,2] <- as.numeric(parameter_table[,2])
@@ -69,7 +72,7 @@ final_table = merge(parameter_table, summary_df, by = "run_code")
 final_table = final_table[order(-final_table$general_performance), ]
 final_table[1:15,]
 
-write.table(final_table,file = "scatterplot_dec_2014.txt", col.names=TRUE,row.names=FALSE,sep=";")
+write.table(final_table,file = "scatterplot_july_2015.txt", col.names=TRUE,row.names=FALSE,sep=";")
 
 
 
