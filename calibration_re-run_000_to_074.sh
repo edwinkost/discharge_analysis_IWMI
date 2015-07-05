208a9214ca128f9b32e67b9d42e9e395fbfad222
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-07-05 21:55:42 +0200

    Update and add batch scripts.

---
+++ calibration_re-run_000_to_074.sh
@@ -0,0 +1,51 @@
+#!/bin/bash                                                                                                                                                                    #SBATCH -N 1                
+#SBATCH -t 59:00         
+#SBATCH -p short              
+
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__0  &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__1  &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__2  &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__3  &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__4  &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__5  &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__6  &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__7  &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__8  &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__9  &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__10 &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__11 &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__12 &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__13 &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__14 &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__15 &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__16 &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__17 &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__18 &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__19 &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__20 &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__21 &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__22 &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__23 &
+wait
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__24 &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__25 &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__26 &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__27 &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__28 &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__29 &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__30 &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__31 &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__32 &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__33 &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__34 &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__35 &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__36 &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__37 &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__38 &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__39 &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__40 &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__41 &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__42 &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__43 &
+python 0_main_analyze_discharge.py /projects/wtrcycle/users/edwinhs/IWMI_calibration/code__a__44 &
+wait
