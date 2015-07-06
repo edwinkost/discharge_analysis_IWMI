5121fbeb20158c859a5e493a6cd78717aef3c3f3
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-07-06 02:05:02 +0200

    Find the best run.

--- check_a_specific_run.R
+++ check_a_specific_run.R
@@ -3,6 +3,12 @@
 folder = "/scratch/edwin/IWMI_calibration/version_01_dec_2014/uncalibrated/code__a__0/analysis/calibration/"
 folder = "/scratch/edwin/30min_22_jun_2015/rerun_for_iwmi/calibration_27_june_2015/code__a__0/analysis/calibration/"
 
+# folder is defined based on the system argument
+args <- commandArgs(trailingOnly = TRUE)
+print(args)
+folder = as.character(args[1])
+print(folder)
+
 # read table containing discharge analysis
 discharge_table_file = paste(folder, "monthly_discharge/summary.txt", sep="") 
 discharge_table = read.table(discharge_table_file, header=T, sep= ";")
