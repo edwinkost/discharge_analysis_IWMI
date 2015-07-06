fda7233da1e52f7823c71ce1030fbba9a4136d7a
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-07-06 09:42:46 +0200

    Find the best run.

--- calculate_performance.R
+++ calculate_performance.R
@@ -5,9 +5,9 @@ ns_discharge[which(ns_discharge < 0.00)] = 0.00
 average_ns_discharge = mean(ns_discharge, na.rm = TRUE)
 #
 baseflow_deviation_relative = abs(baseflow_table$avg_baseflow_deviation/baseflow_table$average_iwmi_opt_baseflow)
-baseflow_deviation_relative[which(baseflow_deviation_relative > 2.00)]   = 2.00
-baseflow_deviation_relative[which(is.na(baseflow_deviation_relative) )]  = 2.00
-baseflow_deviation_relative[which(is.nan(baseflow_deviation_relative) )] = 2.00
+baseflow_deviation_relative[which(baseflow_deviation_relative > 1.00)]   = 1.00
+baseflow_deviation_relative[which(is.na(baseflow_deviation_relative) )]  = 1.00
+baseflow_deviation_relative[which(is.nan(baseflow_deviation_relative) )] = 1.00
 #~ baseflow_deviation_relative = floor(baseflow_deviation_relative*100)/100
 baseflow_deviation = mean(baseflow_deviation_relative, na.rm = FALSE)
 #
