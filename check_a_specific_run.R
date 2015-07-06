cba03e96061c45158a90630f1bde1f2c2c7e7ed7
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2015-07-06 02:10:28 +0200

    Find the best run.

--- check_a_specific_run.R
+++ check_a_specific_run.R
@@ -6,15 +6,15 @@ folder = "/scratch/edwin/30min_22_jun_2015/rerun_for_iwmi/calibration_27_june_20
 # folder is defined based on the system argument
 args <- commandArgs()
 print(args)
-folder = args[4]+"/"
+folder = args[4]
 print(folder)
 
 # read table containing discharge analysis
-discharge_table_file = paste(folder, "monthly_discharge/summary.txt", sep="") 
+discharge_table_file = paste(folder, "/monthly_discharge/summary.txt", sep="") 
 discharge_table = read.table(discharge_table_file, header=T, sep= ";")
 
 # read table containing baseflow analysis
-baseflow_table_file  = paste(folder, "annual_baseflow/baseflow_summary.txt", sep="") 
+baseflow_table_file  = paste(folder, "/annual_baseflow/baseflow_summary.txt", sep="") 
 baseflow_table = read.table(baseflow_table_file, header=T, sep= ";")
 
 # calculate performance values
