3ef5a4997f74cbe5e676a7b6ab935fc4e8067b3e
Edwin Sutanudjaja <sutan101@speedy.geo.uu.nl>
2015-07-06 21:44:30 +0200

    Working on ploting the performance.

---
+++ plot_performance_in_map.py
@@ -0,0 +1,22 @@
+
+import pcraster as pcr
+
+# ldd file name
+ldd_file_name = "/data/hydroworld/PCRGLOBWB20/input30min/routing/lddsound_30min.map"
+
+# working directory (obtained from the argument)
+working_directory 
+
+# set clone
+pcr.setclone(ldd_map)
+
+# station/catchment map
+station_catchment_file_name = "station_map/calibration/calibration_station.map"
+
+# plot in the pcraster format
+table_performance = 
+
+performance = pcr.lookupscalar(table_performance, station_catchment_map)
+
+
+pcrcalc discharge_performance.map = lookupscalaer
